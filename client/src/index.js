0973f165f6089352da2a162f1821b0ca626dc68b
Benjamin Neo <benneost@gmail.com>
2021-10-06 16:19:48 +0800

    installed router & mdb

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
