e14e1fec42824efbbe37be2d2fd4715fd4e43bde
Benjamin Neo <benneost@gmail.com>
2021-10-06 16:56:43 +0800

    Removed unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,26 @@ import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
 import Edit from "./components/edit";
-import Create from "./components/create";
-import RecordList from "./components/recordList";
 import Rewards from "./components/rewards";
+import Home from "./components/home";
 
 const App = () => {
   return (
+    <Rewards/>
+
     // this portion is to allow the app to route to specific paths
-    <BrowserRouter>
-      {/* <Route exact path="/">
-        <Redirect to='/home'/>
-      </Route> */}
+    // <BrowserRouter>
+    //   {/* <Route exact path="/">
+    //     <Redirect to='/home'/>
+    //   </Route> */}
 
-      {/* set the path to which link u want to route to, the component is the function in its respective js file */}
-      <Route path='/rewards' component={Rewards}/>
+    //   {/* set the path to which link u want to route to, the component is the function in its respective js file */}
+    //   <Route path='/rewards' component={Rewards}/>
 
-      <Route path='/home' component={Home}/>
-    </BrowserRouter>
+    //   <Route path='/home' component={Home}/>
+    // </BrowserRouter>
+
+    
   );
 };
 
--- client/src/components/rewards.js
+++ client/src/components/rewards.js
@@ -1,173 +1,73 @@
 import React from 'react';
 import { MDBBtn, MDBContainer, MDBIcon, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBRow, MDBCol, MDBCardSubTitle } from 'mdb-react-ui-kit';
-import LearnerNavbar from '../components/LearnerNavbar';
 
-function LearnerHome() {
-  return (
+
+function Rewards() {
+    return (
     
     <MDBContainer fluid>
-      <title>SPM LEARNER HOMEPAGE</title>
-      {/* this is the navbar thats imported from the respective navbar js*/}
-      <LearnerNavbar/>
+    <title>Rewards</title>
+    {/* this is the navbar thats imported from the respective navbar js*/}
+    {/* <LearnerNavbar/> */}
 
-      {/* can edit whatever u want here */}
-      <div
+    {/* can edit whatever u want here */}
+    <div
         style={{ height: '100vh', marginTop: '120px'}}
-        className= 'px-4'
-      >
+        className= 'px-4, center'
+    >
         
-        {/* Course section */}
+        // {/* Rewards section */}
         <div className='py-4'>
-          <div>
-            <h5 className='mb-3 py-4'>
-              <b>My Courses</b>
-            </h5>
-            <MDBRow>
-              <MDBCol sm='4' style={{marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody className='text-center'>
-                    <MDBCardTitle style={{ paddingTop: '10px'}}><MDBIcon className='ms-1' icon='desktop' size='2x' /></MDBCardTitle>
-                    <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Web Development</MDBCardSubTitle>
-                    <MDBBtn href='#'>Go to Course</MDBBtn>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-              <MDBCol sm='4' style={{marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody className='text-center'>
-                    <MDBCardTitle style={{ paddingTop: '10px'}}><MDBIcon className='ms-1' icon='database' size='2x' /></MDBCardTitle>
-                    <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Database</MDBCardSubTitle>
-                    <MDBBtn href='#'>Go to Course</MDBBtn>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-              <MDBCol sm='4' style={{marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody className='text-center'>
-                    <MDBCardTitle style={{ paddingTop: '10px'}}><MDBIcon className='ms-1' icon='code' size='2x' /></MDBCardTitle>
-                    <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Coding</MDBCardSubTitle>
-                    <MDBBtn sm='2' href='#'>Go to Course</MDBBtn>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-              <MDBCol sm='4' style={{marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody className='text-center'>
-                    <MDBCardTitle style={{ paddingTop: '10px'}}><MDBIcon className='ms-1' icon='camera' size='2x' /></MDBCardTitle>
-                    <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Photography</MDBCardSubTitle>
-                    <MDBBtn href='#'>Go to Course</MDBBtn>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-            </MDBRow>
-          </div>
-
-          {/* Assessments section */}
-          <div className='py-4'>
-            <h5 className='mb-3 py-4'>
-            <b>My Assessments</b>
-            </h5>
-            <MDBRow>
-              <MDBCol sm='6' style={{ maxWidth: '60rem', marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody>
-                    <MDBCardTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Web Development</MDBCardTitle>
-                    <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Due on 26/9/2021</MDBCardSubTitle>
-                    <MDBCardText style={{ paddingTop: '10px', paddingBottom: '10px'}}>
-                      Instructions: Please submit the assignment in a zip file with the necessary files
-                    </MDBCardText>
-                    <MDBBtn href='#'>Go submission page</MDBBtn>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-              <MDBCol sm='6' style={{ maxWidth: '60rem', marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody>
-                    <MDBCardTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Database</MDBCardTitle>
-                    <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Due on 26/9/2021</MDBCardSubTitle>
-                    <MDBCardText style={{ paddingTop: '10px', paddingBottom: '10px'}}>
-                      Instructions: Please submit the assignment in a zip file with the necessary files
-                    </MDBCardText>
-                    <MDBBtn href='#'>Go submission page</MDBBtn>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-              <MDBCol sm='6' style={{ maxWidth: '60rem', marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody>
-                    <MDBCardTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Coding</MDBCardTitle>
-                    <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Due on 26/9/2021</MDBCardSubTitle>
-                    <MDBCardText style={{ paddingTop: '10px', paddingBottom: '10px'}}>
-                      Instructions: Please submit the assignment in a zip file with the necessary files
-                    </MDBCardText>
-                    <MDBBtn href='#'>Go submission page</MDBBtn>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-              <MDBCol sm='6' style={{ maxWidth: '60rem', marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody>
-                    <MDBCardTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Photography</MDBCardTitle>
-                    <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Due on 26/9/2021</MDBCardSubTitle>
-                    <MDBCardText style={{ paddingTop: '10px', paddingBottom: '10px'}}>
-                      Instructions: Please submit the assignment in a zip file with the necessary files
-                    </MDBCardText>
-                    <MDBBtn href='#'>Go to submission page</MDBBtn>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-            </MDBRow>
-          </div>
-
-          {/* Announcement section */}
-          <div className='py-4'>
-            <MDBRow>
-              <MDBCol>
+            <div>
                 <h5 className='mb-3 py-4'>
-                <b>Announcements</b>
+                <b>Rewards</b>
                 </h5>
-              </MDBCol>
-              <MDBCol sm='4'>
-                <form className='d-flex input-group w-auto mb-3 py-4'>
-                <input type='search' className='form-control' placeholder='Search Announcements' aria-label='Search' />
-                <MDBBtn color='primary'>Search</MDBBtn>
-                </form>
-              </MDBCol>
-              <MDBCol></MDBCol>
-              <MDBCol></MDBCol>
-              <MDBCol></MDBCol>
-              <MDBCol></MDBCol>
-              <MDBCol></MDBCol>
-              <MDBCol></MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol sm='4' style={{ marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody className='text-center'>
-                    <MDBCardTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Web Development</MDBCardTitle>
-                    <MDBCardText style={{ paddingTop: '10px', paddingBottom: '10px'}}>
-                      Exam on 19/10/2021. Please be reminded to study and study and study and study to prepare for the exams.
-                    </MDBCardText>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-              <MDBCol sm='4' style={{marginBottom: '10px'}}>
-                <MDBCard>
-                  <MDBCardBody className='text-center'>
-                    <MDBCardTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Coding</MDBCardTitle>
-                    <MDBCardText style={{ paddingTop: '10px', paddingBottom: '10px'}}>
-                      Exam on 19/10/2021. Please be reminded to study and study and study and study to prepare for the exams.
-                    </MDBCardText>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-            </MDBRow>
-          </div>
-
+                <MDBCol>
+                <MDBRow sm='4' style={{marginBottom: '10px', maxWidth: '22rem'}}>
+                    <MDBCard>
+                    <MDBCardImage src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
+                    <MDBCardBody className='text-center'>
+                        <MDBCardTitle style={{ paddingTop: '10px'}}>$2 off Koi</MDBCardTitle>
+                        <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>2,000 points</MDBCardSubTitle>
+                        {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
+                    </MDBCardBody>
+                    </MDBCard>
+                </MDBRow>
+                <MDBRow sm='4' style={{marginBottom: '10px'}}>
+                    <MDBCard>
+                    <MDBCardBody className='text-center'>
+                        <MDBCardTitle style={{ paddingTop: '10px'}}><MDBIcon className='ms-1' icon='database' size='2x' /></MDBCardTitle>
+                        <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Database</MDBCardSubTitle>
+                        <MDBBtn href='#'>Go to Course</MDBBtn>
+                    </MDBCardBody>
+                    </MDBCard>
+                </MDBRow>
+                <MDBRow sm='4' style={{marginBottom: '10px'}}>
+                    <MDBCard>
+                    <MDBCardBody className='text-center'>
+                        <MDBCardTitle style={{ paddingTop: '10px'}}><MDBIcon className='ms-1' icon='code' size='2x' /></MDBCardTitle>
+                        <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Coding</MDBCardSubTitle>
+                        <MDBBtn sm='2' href='#'>Go to Course</MDBBtn>
+                    </MDBCardBody>
+                    </MDBCard>
+                </MDBRow>
+                <MDBRow sm='4' style={{marginBottom: '10px'}}>
+                    <MDBCard>
+                    <MDBCardBody className='text-center'>
+                        <MDBCardTitle style={{ paddingTop: '10px'}}><MDBIcon className='ms-1' icon='camera' size='2x' /></MDBCardTitle>
+                        <MDBCardSubTitle style={{ paddingTop: '10px', paddingBottom: '10px'}}>Photography</MDBCardSubTitle>
+                        <MDBBtn href='#'>Go to Course</MDBBtn>
+                    </MDBCardBody>
+                    </MDBCard>
+                </MDBRow>
+                </MDBCol>
+            </div>
         </div>
-      </div>
+
+        
+    </div>
     </MDBContainer>
-  );
+    );
 }
 
-export default LearnerHome;
+export default Rewards;
