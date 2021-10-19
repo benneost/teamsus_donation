6584dc119c3a72cac47dd10ff6970e3cbe578925
Benjamin Neo <benneost@gmail.com>
2021-10-19 22:07:13 +0800

    benficiary images

---
+++ client/src/components/beneficiaryimage.js
@@ -0,0 +1,28 @@
+import image1 from '../beneficiaryimages/pregnant.jpeg';
+import image2 from '../beneficiaryimages/arts.jpeg';
+import image3 from '../beneficiaryimages/environment.jpeg';
+import image4 from '../beneficiaryimages/health.jpeg';
+import image5 from '../beneficiaryimages/edu.jpeg';
+import image6 from '../beneficiaryimages/health1.jpeg';
+import image7 from '../beneficiaryimages/poverty.jpeg';
+import image8 from '../beneficiaryimages/food.jpeg';
+import image9 from '../beneficiaryimages/edu1.jpeg';
+import image10 from '../beneficiaryimages/health2.jpeg';
+
+const beneficiaryimage = [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10
+    // require('images/wooly3'),
+    // require('images/wooly4'),
+    // require('images/wooly5'),
+];
+
+export default beneficiaryimage;
