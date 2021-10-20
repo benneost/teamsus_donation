fb1a8a6296e48ea7ef327109df30c775c39cef2f
tanya <tanyakhoo.2020@scis.smu.edu.sg>
2021-10-20 21:59:45 +0800

    Update rewards.js

--- client/src/components/rewards.js
+++ client/src/components/rewards.js
@@ -39,20 +39,11 @@ function Rewards() {
     {/* <LearnerNavbar/> */}
 
     {/* can edit whatever u want here */}
-    <div
-        style={{ height: '100vh', marginTop: '120px'}}
-        className= 'px-4 d-flex align-items-baseline justify-content-center'
-    >
+
+    <div class="row">
+        <div class="col-sm-6">
         
-        {/* Rewards section */}
-        <div className='py-4'>
-            <div>
-                <h2 className='mb-3 py-4'>
-                <b>Rewards</b>
-                </h2>
-                <MDBCol>
-                <MDBRow sm='4' style={{marginBottom: '10px', maxWidth: '22rem'}}>
-                    <MDBCard onClick={toggleShow}>
+        <MDBCard onClick={toggleShow} style={{margin: '30px'}}>
                     <MDBCardImage src='https://images.deliveryhero.io/image/fd-th/LH/w7wn-hero.jpg' position='top' alt='...' />
                     <MDBCardBody className='text-center'>
                         <MDBCardTitle style={{ paddingTop: '5px'}}>$2 off Koi</MDBCardTitle>
@@ -60,51 +51,8 @@ function Rewards() {
                         {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
                     </MDBCardBody>
                     </MDBCard>
-                </MDBRow>
-                <MDBRow sm='4' style={{marginBottom: '10px', maxWidth: '22rem'}}>
-                    <MDBCard>
-                    <MDBCardImage src='https://images.dailyhive.com/20191119104317/food_with_howie.jpeg' position='top' alt='...' />
-                    <MDBCardBody className='text-center'>
-                        <MDBCardTitle style={{ paddingTop: '5px'}}>1 Free Milk Tea at GongCha</MDBCardTitle>
-                        <MDBCardSubTitle style={{ paddingTop: '5px', paddingBottom: '5px'}}>5,000 points</MDBCardSubTitle>
-                        {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
-                    </MDBCardBody>
-                    </MDBCard>
-                </MDBRow>
-                <MDBRow sm='4' style={{marginBottom: '10px', maxWidth: '22rem'}}>
-                    <MDBCard>
-                    <MDBCardImage src='https://www.8days.sg/image/12680056/16x9/1920/1080/4386bc4300a3f41c2206e3789e168897/XS/liho-diy5.jpg' position='top' alt='...' />
-                    <MDBCardBody className='text-center'>
-                        <MDBCardTitle style={{ paddingTop: '5px'}}>$2 off LiHo</MDBCardTitle>
-                        <MDBCardSubTitle style={{ paddingTop: '5px', paddingBottom: '5px'}}>2,000 points</MDBCardSubTitle>
-                        {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
-                    </MDBCardBody>
-                    </MDBCard>
-                </MDBRow>
-                <MDBRow sm='4' style={{marginBottom: '10px', maxWidth: '22rem'}}>
-                    <MDBCard>
-                    <MDBCardImage src='https://static.straitstimes.com.sg/s3fs-public/articles/2019/02/12/colin-cd-12.jpg' position='top' alt='...' />
-                    <MDBCardBody className='text-center'>
-                        <MDBCardTitle style={{ paddingTop: '5px'}}>$5 off An Acai Affair</MDBCardTitle>
-                        <MDBCardSubTitle style={{ paddingTop: '5px', paddingBottom: '5px'}}>5,000 points</MDBCardSubTitle>
-                        {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
-                    </MDBCardBody>
-                    </MDBCard>
-                </MDBRow>
-                <MDBRow sm='4' style={{marginBottom: '10px', maxWidth: '22rem'}}>
-                    <MDBCard>
-                    <MDBCardImage src='https://www.capitaland.com/content/dam/capitaland-media-library/retail/Singapore/Singapore/Plaza%20Singapura/yakun%20carousel.jpg.transform/cap-midres/image.jpg' position='top' alt='...' />
-                    <MDBCardBody className='text-center'>
-                        <MDBCardTitle style={{ paddingTop: '5px'}}>$2 off Set Meal at Ya Kun</MDBCardTitle>
-                        <MDBCardSubTitle style={{ paddingTop: '5px', paddingBottom: '5px'}}>1,500 points</MDBCardSubTitle>
-                        {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
-                    </MDBCardBody>
-                    </MDBCard>
-                </MDBRow>
-                
-                </MDBCol>
 
-                <MDBModal tabIndex='-1' show={centredModal} getOpenState={(e: any) => setCentredModal(e)} >
+                    <MDBModal tabIndex='-1' show={centredModal} getOpenState={(e: any) => setCentredModal(e)} >
                     <MDBModalDialog centered size="sm">
                         <MDBModalContent>
                         <MDBModalHeader>
@@ -147,9 +95,63 @@ function Rewards() {
                         </MDBModalContent>
                     </MDBModalDialog>
                 </MDBModal>
-            </div>
+                    
+        </div>
+        
+        <div class="col-sm-6">
+        
+        <MDBCard style={{margin: '30px'}}>
+                    <MDBCardImage src='https://images.dailyhive.com/20191119104317/food_with_howie.jpeg' position='top' alt='...' />
+                    <MDBCardBody className='text-center'>
+                        <MDBCardTitle style={{ paddingTop: '5px'}}>1 Free Milk Tea at GongCha</MDBCardTitle>
+                        <MDBCardSubTitle style={{ paddingTop: '5px', paddingBottom: '5px'}}>5,000 points</MDBCardSubTitle>
+                        {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
+                    </MDBCardBody>
+                    </MDBCard>
+        </div>
+        
+        <div class="col-sm-6">
+        
+            <MDBCard style={{margin: '30px'}}>
+                    <MDBCardImage src='https://www.8days.sg/image/12680056/16x9/1920/1080/4386bc4300a3f41c2206e3789e168897/XS/liho-diy5.jpg' position='top' alt='...' />
+                    <MDBCardBody className='text-center'>
+                        <MDBCardTitle style={{ paddingTop: '5px'}}>$2 off LiHo</MDBCardTitle>
+                        <MDBCardSubTitle style={{ paddingTop: '5px', paddingBottom: '5px'}}>2,000 points</MDBCardSubTitle>
+                        {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
+                    </MDBCardBody>
+                    </MDBCard>
         </div>
 
+        <div class="col-sm-6">
+        
+        <MDBCard style={{margin: '30px'}}>
+                    <MDBCardImage src='https://static.straitstimes.com.sg/s3fs-public/articles/2019/02/12/colin-cd-12.jpg' position='top' alt='...' />
+                    <MDBCardBody className='text-center'>
+                        <MDBCardTitle style={{ paddingTop: '5px'}}>$5 off An Acai Affair</MDBCardTitle>
+                        <MDBCardSubTitle style={{ paddingTop: '5px', paddingBottom: '5px'}}>5,000 points</MDBCardSubTitle>
+                        {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
+                    </MDBCardBody>
+                    </MDBCard>
+        </div>
+
+        <div class="col-sm-6">
+        
+        <MDBCard style={{margin: '30px'}}>
+                    <MDBCardImage src='https://www.capitaland.com/content/dam/capitaland-media-library/retail/Singapore/Singapore/Plaza%20Singapura/yakun%20carousel.jpg.transform/cap-midres/image.jpg' position='top' alt='...' />
+                    <MDBCardBody className='text-center'>
+                        <MDBCardTitle style={{ paddingTop: '5px'}}>$2 off Set Meal at Ya Kun</MDBCardTitle>
+                        <MDBCardSubTitle style={{ paddingTop: '5px', paddingBottom: '5px'}}>1,500 points</MDBCardSubTitle>
+                        {/* <MDBBtn href='#'>Go to Course</MDBBtn> */}
+                    </MDBCardBody>
+                    </MDBCard>
+        </div>
+        
+    </div>
+
+    <div
+        style={{ height: '100vh', marginTop: '120px'}}
+        className= 'px-4 d-flex align-items-baseline justify-content-center'
+    >
         
     </div>
     </MDBContainer>
